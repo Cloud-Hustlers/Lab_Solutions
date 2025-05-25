fda52cd8743fdfa8f976e16cabceb4fd7f20dfc3
Cloud-Hustlers <help.cloudhustlers@gmail.com>
2025-05-25 14:55:55 +0530

    Update and rename hustlergsp752.sh to gsp752.sh

--- Managing Terraform State/gsp752.sh
+++ Managing Terraform State/gsp752.sh
@@ -1,3 +1,25 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
 curl -LO https://raw.githubusercontent.com/Cloud-Hustlers/content/main/creativity/welcome.sh
 if [[ ! -f welcome.sh ]]; then
   echo "Download failed or file not found!"
@@ -7,6 +29,7 @@ chmod +x welcome.sh
 ./welcome.sh
 
 
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
 cat > main.tf <<EOF_END
 
@@ -27,11 +50,9 @@ terraform {
 }
 EOF_END
 
-
 terraform init
 
 terraform apply --auto-approve
-
 
 cat > main.tf <<EOF_END
 
@@ -53,12 +74,13 @@ terraform {
 }
 EOF_END
 
-
 yes | terraform init -migrate-state
-
 
 gsutil label ch -l "key:value" gs://$DEVSHELL_PROJECT_ID
 
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
 curl -LO https://raw.githubusercontent.com/Cloud-Hustlers/content/refs/heads/main/creativity/subscribe.sh
 chmod +x subscribe.sh
 ./subscribe.sh
