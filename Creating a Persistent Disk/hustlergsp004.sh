4d571b646cd3b39fdda9c80d68231bc6578003db
Cloud-Hustlers <help.cloudhustlers@gmail.com>
2025-05-30 13:45:31 +0530

    Update hustlergsp004.sh

--- Creating a Persistent Disk/hustlergsp004.sh
+++ Creating a Persistent Disk/hustlergsp004.sh
@@ -1,3 +1,26 @@
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
 curl -LO https://raw.githubusercontent.com/Cloud-Hustlers/content/main/creativity/welcome.sh
 if [[ ! -f welcome.sh ]]; then
   echo "Download failed or file not found!"
@@ -7,6 +30,12 @@ chmod +x welcome.sh
 ./welcome.sh
 
 
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+export REGION="${ZONE%-*}"
+
+gcloud config set compute/zone $ZONE
+gcloud config set compute/region $REGION
 
 gcloud compute instances create gcelab --zone $ZONE --machine-type e2-standard-2
 
@@ -15,12 +44,23 @@ gcloud compute disks create mydisk --size=200GB \
 
 gcloud compute instances attach-disk gcelab --disk mydisk --zone $ZONE
 
+cat > prepare_disk.sh <<'EOF_END'
 
-gcloud compute ssh gcelab --zone $ZONE --quiet --command "sudo mkdir /mnt/mydisk &&
-  sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 &&
-  sudo mount -o discard,defaults /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 /mnt/mydisk &&
-  echo '/dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 /mnt/mydisk ext4 defaults 1 1' | sudo tee -a /etc/fstab"
+ls -l /dev/disk/by-id/
 
+sudo mkdir /mnt/mydisk
+
+sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1
+
+sudo mount -o discard,defaults /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 /mnt/mydisk
+
+EOF_END
+
+gcloud compute scp prepare_disk.sh gcelab:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh gcelab --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
 
 
