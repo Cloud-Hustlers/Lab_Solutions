4221569c1c59ca93b3e53e21e0685a7a2640583d
Cloud-Hustlers <help.cloudhustlers@gmail.com>
2025-05-30 18:31:29 +0530

    Update form-3.sh

--- Get Started with PubSub Challenge Lab/form-3.sh
+++ Get Started with PubSub Challenge Lab/form-3.sh
@@ -34,16 +34,16 @@ echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
 export MSG_BODY='Hello World!'
 
-gcloud pubsub topics create cloud-pubsub-topic
+gcloud pubsub topics create gcloud-pubsub-topic
 
-gcloud pubsub subscriptions create cloud-pubsub-subscription --topic=cloud-pubsub-topic
+gcloud pubsub subscriptions create cloud-pubsub-subscription --topic=gcloud-pubsub-topic
 
 gcloud services enable cloudscheduler.googleapis.com
 
 gcloud scheduler jobs create pubsub cron-scheduler-job \
   --location=$LOCATION \
   --schedule="* * * * *" \
-  --topic=cloud-pubsub-topic \
+  --topic=gcloud-pubsub-topic \
   --message-body="Hello World!"
 
 gcloud pubsub subscriptions pull cloud-pubsub-subscription --limit 5
