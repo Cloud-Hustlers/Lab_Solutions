e006af9dc594239a5e151aaa079de95ddf3d1818
Cloud-Hustlers <help.cloudhustlers@gmail.com>
2025-05-30 16:45:27 +0530

    Create GSP401.sh

---
+++ Cloud Scheduler: Qwik Start/GSP401.sh
@@ -0,0 +1,50 @@
+
+
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+
+curl -LO https://raw.githubusercontent.com/Cloud-Hustlers/content/main/creativity/welcome.sh
+if [[ ! -f welcome.sh ]]; then
+  echo "Download failed or file not found!"
+  exit 1
+fi
+chmod +x welcome.sh
+./welcome.sh
+
+
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable cloudscheduler.googleapis.com
+
+gcloud pubsub topics create cron-topic
+
+gcloud pubsub subscriptions create cron-sub --topic cron-topic
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+
+curl -LO https://raw.githubusercontent.com/Cloud-Hustlers/content/refs/heads/main/creativity/subscribe.sh
+chmod +x subscribe.sh
+./subscribe.sh
