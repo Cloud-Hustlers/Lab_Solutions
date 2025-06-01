cecee35e988796dbf59657c0b6adc417e73d9794
Cloud-Hustlers <help.cloudhustlers@gmail.com>
2025-06-01 17:55:34 +0530

    Update hustlergsp903.sh

--- Stream Processing with Cloud Pub:Sub and Dataflow Qwik Start/hustlergsp903.sh
+++ Stream Processing with Cloud Pub:Sub and Dataflow Qwik Start/hustlergsp903.sh
@@ -1,3 +1,24 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+
 curl -LO https://raw.githubusercontent.com/Cloud-Hustlers/content/main/creativity/welcome.sh
 if [[ ! -f welcome.sh ]]; then
   echo "Download failed or file not found!"
@@ -6,7 +27,7 @@ fi
 chmod +x welcome.sh
 ./welcome.sh
 
-
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
 gcloud config set compute/region $REGION
 
@@ -26,6 +47,8 @@ gcloud pubsub topics create $TOPIC_ID
 
 if [ "$REGION" == "us-central1" ]; then
   gcloud app create --region us-central
+elif [ "$REGION" == "europe-west1" ]; then
+  gcloud app create --region europe-west
 else
   gcloud app create --region "$REGION"
 fi
@@ -33,26 +56,36 @@ fi
 gcloud scheduler jobs create pubsub publisher-job --schedule="* * * * *" \
     --topic=$TOPIC_ID --message-body="Hello!"
 
-gcloud scheduler jobs run publisher-job
+sleep 60
 
-git clone https://github.com/GoogleCloudPlatform/java-docs-samples.git
-cd java-docs-samples/pubsub/streaming-analytics
+gcloud scheduler jobs run publisher-job --location=$REGION
 
-mvn compile exec:java \
--Dexec.mainClass=com.examples.pubsub.streaming.PubSubToGcs \
--Dexec.cleanupDaemonThreads=false \
--Dexec.args=" \
-    --project=$PROJECT_ID \
-    --region=$REGION \
-    --inputTopic=projects/$PROJECT_ID/topics/$TOPIC_ID \
-    --output=gs://$BUCKET_NAME/samples/output \
-    --runner=DataflowRunner \
-    --windowSize=2"
+sleep 60
 
+gcloud scheduler jobs run publisher-job --location=$REGION
 
+cat > automate_commands.sh <<EOF_END
+#!/bin/bash
 
+git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
+cd python-docs-samples/pubsub/streaming-analytics
+pip install -U -r requirements.txt
+python PubSubToGCS.py \
+--project=$PROJECT_ID \
+--region=$REGION \
+--input_topic=projects/$PROJECT_ID/topics/$TOPIC_ID \
+--output_path=gs://$BUCKET_NAME/samples/output \
+--runner=DataflowRunner \
+--window_size=2 \
+--num_shards=2 \
+--temp_location=gs://$BUCKET_NAME/temp
+EOF_END
 
+chmod +x automate_commands.sh
 
+docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e BUCKET_NAME=$BUCKET_NAME -e PROJECT_ID=$PROJECT_ID -e REGION=$REGION -e TOPIC_ID=$TOPIC_ID -v $(pwd)/automate_commands.sh:/automate_commands.sh python:3.7 /bin/bash -c "/automate_commands.sh"
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
 
 
 
