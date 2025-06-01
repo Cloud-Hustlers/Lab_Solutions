4867b9a72a1e2072ce8ae57ecac3ea067fea35a2
Cloud-Hustlers <help.cloudhustlers@gmail.com>
2025-06-01 17:18:58 +0530

    Update hustlergsp294.sh

--- Introduction to APIs in Google Cloud/hustlergsp294.sh
+++ Introduction to APIs in Google Cloud/hustlergsp294.sh
@@ -27,7 +27,7 @@ curl -X POST --data-binary @values.json \
 #TASK 5
 
 
-wget https://github.com/Cloud-Hustlers/Lab_Solutions/blob/master/Extract%20Analyze%20and%20Translate%20Text%20from%20Images%20with%20the%20Cloud%20ML%20APIs/sign.jpg
+wget https://github.com/Cloud-Hustlers/Lab_Solutions/blob/main/Extract%20Analyze%20and%20Translate%20Text%20from%20Images%20with%20the%20Cloud%20ML%20APIs/sign.jpg
 
 mv sign.jpg demo-image.png
 
