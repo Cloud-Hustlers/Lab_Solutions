5d98490fd42af96a4665450926a3dde68d994ab0
Cloud-Hustlers <help.cloudhustlers@gmail.com>
2025-06-07 10:31:54 +0530

    Update gsp222.sh

--- It Speaks! Create Synthetic Speech Using Text-to-Speech/gsp222.sh
+++ It Speaks! Create Synthetic Speech Using Text-to-Speech/gsp222.sh
@@ -1,3 +1,11 @@
+curl -LO https://raw.githubusercontent.com/Cloud-Hustlers/content/main/creativity/welcome.sh
+if [[ ! -f welcome.sh ]]; then
+  echo "Download failed or file not found!"
+  exit 1
+fi
+chmod +x welcome.sh
+./welcome.sh
+
 gcloud services enable texttospeech.googleapis.com --project=$DEVSHELL_PROJECT_ID
 
 sudo apt-get install -y virtualenv
@@ -14,3 +22,6 @@ export PROJECT_ID=$(gcloud config get-value project)
 gcloud iam service-accounts keys create tts-qwiklab.json --iam-account tts-qwiklab@$PROJECT_ID.iam.gserviceaccount.com
 
 export GOOGLE_APPLICATION_CREDENTIALS=tts-qwiklab.json
+curl -LO https://raw.githubusercontent.com/Cloud-Hustlers/content/refs/heads/main/creativity/subscribe.sh
+chmod +x subscribe.sh
+./subscribe.sh
