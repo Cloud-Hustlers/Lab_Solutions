f0c68abf875ff4ec125d1057777841b26ecfc1b8
Cloud-Hustlers <help.cloudhustlers@gmail.com>
2025-06-07 11:53:21 +0530

    Update task1.sh

--- Speaking with a Webpage  Streaming Speech Transcripts/task1.sh
+++ Speaking with a Webpage  Streaming Speech Transcripts/task1.sh
@@ -8,12 +8,6 @@ chmod +x welcome.sh
 
 
 
-
-
-
-
-
-
 gcloud compute instances create speaking-with-a-webpage --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD  --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --create-disk=auto-delete=yes,boot=yes,device-name=speaking-with-a-webpage,image=projects/debian-cloud/global/images/debian-11-bullseye-v20230711,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
 
 sleep 20
